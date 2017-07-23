2584f6e976fd5ae36e94667233973a7110a30ac0
amirouche <amirouche+dev@hypermove.net>
2017-07-24 00:20:11 +0200

    add login with auth (wip)

--- socialite/migrations/20170723_01_PflB5-initial-schema.sql
+++ socialite/migrations/20170723_01_PflB5-initial-schema.sql
@@ -1,1 +1,6 @@
-CREATE TABLE bar (test TEXT, test2 INTEGER, test3 TEXT);
+CREATE TABLE user (
+       id SERIAL PRIMARY KEY NOT NULL,
+       username TEXT UNIQUE NOT NULL,
+       password TEXT NOT NULL
+);
+       
