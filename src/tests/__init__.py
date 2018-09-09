584ee1d3ceb149ee0e481467837e62405cda6210
Amirouche <amirouche@hypermove.net>
2018-09-09 22:08:57 +0200

    tests: display logs with DEBUG=DEBUG env variable

---
+++ src/tests/__init__.py
@@ -0,0 +1,9 @@
+import logging
+import os
+
+import daiquiri
+
+
+level_name = os.environ.get('DEBUG', 'INFO')
+level = getattr(logging, level_name)
+daiquiri.setup(level=level, outputs=('stderr',))
