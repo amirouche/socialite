fc714bc2d9caa785b646628558028c207c496e5a
Amirouche <amirouche@hypermove.net>
2018-09-12 22:04:33 +0200

    socialite: move to spark (wip)

--- src/socialite/stream.py
+++ src/socialite/stream.py
@@ -5,106 +5,80 @@ import daiquiri
 from mistune import Markdown
 from aiohttp import web
 
-from socialite import collection
 from socialite import fdb
+from socialite import sparky
 from socialite.user import user_by_username
-from socialite.user import user_by_uid
 
 
 log = daiquiri.getLogger(__name__)
-
-
-STREAM = collection.Collection.STREAM
-USERS = collection.Collection.USERS
-FOLLOWING = collection.Collection.FOLLOWING
 
 
 markdown = Markdown()
 
 
 @fdb.transactional
-async def items_for_user(tr, user):
-    # TODO: optimize using an index or something
-    items = await collection.all(tr, STREAM)
-    out = list()
-    for item in items:
-        if (item['emitter']['kind'] == 'user' and item['emitter']['uid'] == user['uid'].hex):
-            out.append(item)
-    out.sort(key=lambda x: x['modified_at'], reverse=True)
+async def expressions_for_user(tr, username):
+    patterns = [
+        ('actor', sparky.var('actor'), 'name', username),
+        ('stream', sparky.var('expression'), 'html', sparky.var('html')),
+        ('stream', sparky.var('expression'), 'actor', sparky.var('actor')),
+        ('stream', sparky.var('expression'), 'modified-at', sparky.var('modified-at')),
+    ]
+    out = await sparky.where(tr, *patterns)
+    out.sort(key=lambda x: x['modified-at'], reverse=True)
+    out = out[:100]
     return out
 
 
-async def items(request):
+async def expressions(request):
     username = request.match_info['username']
     log.debug("Stream for username='%s'", username)
-    try:
-        user = await user_by_username(request.app['db'], username)
-    except IndexError:
-        raise web.HTTPNotFound()
-    else:
-        items = await items_for_user(request.app['db'], user)
-        context = {
-            "settings": request.app["settings"],
-            "user": user,
-            "items": items,
-            "current_user": request.user,
-        }
-        return request.app.render("stream/items.jinja2", request, context)
+    expressions = await expressions_for_user(request.app['db'], username)
+    context = {
+        "settings": request.app["settings"],
+        "username": username,
+        "expressions": expressions,
+        "current_user": request.user,
+    }
+    return request.app.render("stream/user.jinja2", request, context)
 
 
 @fdb.transactional
 async def stream(tr, user):
-    """Fetch latest items of things that ``user`` follows"""
+    """Fetch latest expressions of things that ``user`` follows"""
     # fetch followee aka. what user follows
-    query = {
-        'follower_kind': 'user',
-        'follower_uid': user['uid'].hex,
-    }
-    followees = await collection.query(tr, FOLLOWING, **query)
-    log.critical(followees)
-    followees = [(x['followee_kind'], x['followee_uid']) for x in followees]
-    followees.append(('user', user["uid"].hex))
-    # fetch items for the followees aka. filter
-    items = await collection.all(tr, STREAM)
-    out = []
-    for item in items:
-        key = (item['emitter']['kind'], item["emitter"]['uid'])
-        if key in followees:
-            out.append(item)
-    out.sort(key=lambda x: x['modified_at'], reverse=True)
+    patterns = [
+        ('stream', sparky.var('following'), 'follower', user),
+        ('stream', sparky.var('following'), 'followee', sparky.var('followee')),
+        ('stream', sparky.var('expression'), 'html', sparky.var('html')),
+        ('stream', sparky.var('expression'), 'user', sparky.var('followee')),
+        ('stream', sparky.var('expression'), 'modified-at', sparky.var('modified-at')),
+        ('actor', sparky.var('followee'), 'name', sparky.var('name')),
+    ]
+    out = await sparky.where(tr, *patterns)
+    out.sort(key=lambda x: x['modified-at'], reverse=True)
     out = out[:100]
-    # fetch and embed users aka. join USER collection
-    for item in out:
-        uid = item['emitter']['uid']
-        uid = UUID(hex=uid)
-        user = await user_by_uid(tr, uid)
-        item['user'] = user
     return out
 
 
 async def stream_get(request):
-    items = await stream(request.app["db"], request.user)
-    context = {"settings": request.app["settings"], "items": items}
+    expressions = await stream(request.app["db"], request.user['uid'])
+    context = {"settings": request.app["settings"], "expressions": expressions}
     return request.app.render("stream/stream.jinja2", request, context)
 
 
 @fdb.transactional
-async def expression_insert(tr, uid, expression, html):
+async def expression_insert(tr, user, expression, html):
     now = int(time())
-    document = {
-        'emitter': {
-            'uid': uid.hex,
-            'kind': 'user',
-        },
-        'source': {
-            'kind': 'expression',
-            'expression': expression,
-        },
-        'html': html,
-        'created_at': now,
-        'modified_at': now,
-    }
-    uid = await collection.insert(tr, STREAM, **document)
+    uid = sparky.random_uid(tr)
+    tuples = [
+        ('stream', uid, 'html', html),
+        ('stream', uid, 'expression', expression),
+        ('stream', uid, 'user', user),
+        ('stream', uid, 'modified-at', now),
+        ('stream', uid, 'created-at', now),
+    ]
+    await sparky.add(tr, *tuples)
     return uid
 
 
