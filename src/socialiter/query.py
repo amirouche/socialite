952d7e1b29a69515e9a1f693bbd18e1acb740be9
Amirouche <amirouche.boubekki@gmail.com>
2019-01-31 09:26:42 +0100

    socialiter: query: add 'bing(app, query)'

--- src/socialiter/query.py
+++ src/socialiter/query.py
@@ -1,7 +1,14 @@
+import logging
+from urllib.parse import urlencode
+from lxml.html import fromstring as string2html
+
 import trafaret as t
 from aiohttp import web
 
 from socialiter import feed
+
+
+log = logging.getLogger(__name__)
 
 
 async def query(request):
@@ -22,3 +29,39 @@ async def query(request):
     # nominal case
     context = {"settings": request.app["settings"], "feed": out, "url": url}
     return request.app.render("feed/home.jinja2", request, context)
+
+
+async def bing(app, query):
+    """Query bing.com for hits that match `query`"""
+    base = 'https://www.bing.com/search?q='
+    url = base + urlencode(query)
+    response = await app['session'].get(url)
+    if response.status == 200:
+        string = await response.text()
+        try:
+            hits = _bing_parse(string)
+        except Exception:
+            log.exception('Failed to parse bing results: %r', response)
+            return list()
+        else:
+            return hits
+    else:
+        log.error('Bing search failed: %r', response)
+        return list()
+
+
+def _bing_parse(string):
+    out = []
+    html = string2html(string)
+    results = html.xpath('//ol[@id="b_results"]/li[@class="b_algo"]')
+    for result in results:
+        hit = _bing_parse_one(result)
+        out.append(hit)
+    return out
+
+
+def _bing_parse_one(result):
+    title = result.xpath('.//h2/a/text()')[0]
+    url = result.xpath('.//h2/a/@href')[0]
+    description = result.xpath('.//*[@class="b_caption"]//p/text()')[0]
+    return dict(title=title, url=url, description=description)
